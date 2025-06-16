f44a6d262ac7de07adfd8d34b996619c3abb20b8
Aditya <aditya007r29@gmail.com>
2025-06-16 19:08:31 +0530

    minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,6 +11,16 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['dayjs'],
+    },
+  },
+  resolve: {
+    alias: {
+      'dayjs': 'dayjs',
     },
   },
 })
