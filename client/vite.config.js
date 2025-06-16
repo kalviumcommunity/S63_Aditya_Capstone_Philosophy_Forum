123050682f896a2b485e05914ffee62a494060d6
Aditya <aditya007r29@gmail.com>
2025-06-16 19:31:39 +0530

    netlify fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,11 +15,24 @@ export default defineConfig({
     },
   },
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks: {
+          vendor: ['dayjs'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      'dayjs': path.resolve(__dirname, 'node_modules/dayjs'),
     },
   },
   optimizeDeps: {
     include: ['dayjs'],
+    exclude: [],
   },
 })
